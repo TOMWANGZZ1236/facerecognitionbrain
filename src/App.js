b82577517bb2e26233c69a43ce9ad40bdcfbc2df
Tom Wang <tomwangzz1236@gmail.com>
2024-10-27 21:43:52 -0400

    new frontend updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import ParticlesBg from 'particles-bg'
+
 
 function App() {
+
   return (
     <div className="App">
+      <ParticlesBg type="cobweb" bg={true} num = {300} /> 
       <Navigation/>
       <Logo/>
+      <Rank/>
+      <ImageLinkForm/>
+      
     </div>
   );
 }
