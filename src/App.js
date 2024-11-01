b7b3acf748f85899094b195ac41be12eeb1260be
Tom Wang <tomwangzz1236@gmail.com>
2024-10-31 20:50:27 -0400

    Added bounding boxes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 // of the image we want as an input.
 
 const apiconfiguration =  (imageUrl) => {
-  const PAT = '914a9169c8744e4a84605d7accab8c11';
+  const PAT = '';
   const USER_ID = 'tomwangwaterloo';
   const APP_ID = 'SmartBrainTW';
   const IMAGE_URL = imageUrl;
@@ -53,37 +53,63 @@ class App extends Component {
     this.state = {
       input : '',
       imageUrl : '',
+      box : {},
     }
+  }
+  calculateFaceLocations = (data) => {
+    const clarifaiFace = data[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    // console.log(clarifaiFace.bottom_row);
+    // console.log(clarifaiFace.top_row);
+    return {
+      leftCol : clarifaiFace.left_col * width,
+      topRow : clarifaiFace.top_row * height,
+      bottomRow : Number(height - clarifaiFace.bottom_row * height),
+      rightCol : Number(width - clarifaiFace.right_col * width),      
+    }
+    
+  }
+  displayFaceBox = (box) => {
+    this.setState({box : box});
   }
   onInputChange = (event) => {
     this.setState({input : event.target.value});
-    console.log('Success');
   }
-  onClickChange = () => {
+  onClickChange = ()  => {
     this.setState({imageUrl: this.state.input});
     fetch("https://cors-anywhere.herokuapp.com/https://api.clarifai.com/v2/models/face-detection/outputs", 
     apiconfiguration(this.state.input))
     .then(response => response.json())
     .then(result => {
+      console.log(result);
       const regions = result.outputs[0].data.regions;
-      regions.forEach(region => {
-          // Accessing and rounding the bounding box values
-          const boundingBox = region.region_info.bounding_box;
-          const topRow = boundingBox.top_row.toFixed(3);
-          const leftCol = boundingBox.left_col.toFixed(3);
-          const bottomRow = boundingBox.bottom_row.toFixed(3);
-          const rightCol = boundingBox.right_col.toFixed(3);
+      return regions;
+      // regions.forEach(region => {
+      //     // Accessing and rounding the bounding box values
+      //     const boundingBox = region.region_info.bounding_box;
+      //     const topRow = boundingBox.top_row.toFixed(3);
+      //     const leftCol = boundingBox.left_col.toFixed(3);
+      //     const bottomRow = boundingBox.bottom_row.toFixed(3);
+      //     const rightCol = boundingBox.right_col.toFixed(3);
 
-          region.data.concepts.forEach(concept => {
-              // Accessing and rounding the concept value
-              const name = concept.name;
-              const value = concept.value.toFixed(4);
+      //     // region.data.concepts.forEach(concept => {
+      //     //     // Accessing and rounding the concept value
+      //     //     const name = concept.name;
+      //     //     const value = concept.value.toFixed(4);
 
-              console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);   
-          });
-      });
+      //     //     console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);   
+              
+      //     // });
+      //     this.displayFaceBox({topRow : topRow, leftCol: leftCol, bottomRow: bottomRow, rightCol: rightCol});
+          
+      // });
     })
+    .then(data => this.displayFaceBox(this.calculateFaceLocations(data)))
     .catch(error => console.log('error', error));
+
+    console.log(this.state.box);
   }
   render() {return (
     <div className="App">
@@ -92,7 +118,7 @@ class App extends Component {
       <Logo/>
       <Rank/>
       <ImageLinkForm onInputChange = {this.onInputChange} onClickChange= {this.onClickChange}/>
-      <FaceRecognition imageSource = {this.state.imageUrl}/>
+      <FaceRecognition imageSource = {this.state.imageUrl} box = {this.state.box}/>
     </div>
   );
 }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './FaceRecognition.css';
 
 
-const FaceRecognition = ({imageSource}) => {
+const FaceRecognition = ({imageSource, box}) => {
     return(
         <div className = 'center ma'>
             <div className = 'absolute mt2'>
-                <img alt = '' src = {imageSource}  width = '500px' height = 'auto'/>
+                <img id = 'inputImage' alt = '' src = {imageSource}  width = '500px' height = 'auto'/>
+                
+                <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     );
